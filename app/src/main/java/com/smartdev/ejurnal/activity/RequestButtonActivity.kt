504e16af5c5e4da8d6b9f1c790329dd393c4aa4a
pharmalink <dominikusvieri9@gmail.com>
2022-02-23 16:29:55 +0700

    First Commit

--- app/src/main/java/com/smartdev/ejurnal/activity/RequestButtonActivity.kt
+++ app/src/main/java/com/smartdev/ejurnal/activity/RequestButtonActivity.kt
@@ -3,7 +3,7 @@ package com.smartdev.ejurnal.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.smartdev.ejurnal.R
-
+//button
 class RequestButtonActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
